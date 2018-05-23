a4e11f95ed4b462d4b6e38260b37cf6a903ff797
Rohit_Sonawane <rohit2790@gmail.com>
2018-05-22 23:08:34 -0400

    adding test cases for transfer function

--- tests/customer_tests.py
+++ tests/customer_tests.py
@@ -1,5 +1,5 @@
-from nose.tools import assert_equals, nottest
-
+from nose.tools import assert_equals, nottest, raises
+from bank import Bank
 from account import Account, CHECKING, SAVINGS
 from customer import Customer
 
@@ -34,3 +34,42 @@ def test_threeAccounts():
     oscar = Customer("Oscar").openAccount(Account(SAVINGS))
     oscar.openAccount(Account(CHECKING))
     assert_equals(oscar.numAccs(), 3)
+
+bank = Bank()
+checkingAccount = Account(CHECKING)
+savingAccount = Account(SAVINGS)
+bill = Customer("Bill")
+bill.openAccount(checkingAccount)
+bill.openAccount(savingAccount)
+bank.addCustomer(bill)
+checkingAccount.deposit(100.0)
+checkingAccount.deposit(500.0)
+checkingAccount.deposit(2000.0)
+savingAccount.deposit(900.0)
+
+from_acc = checkingAccount
+to_acc = savingAccount
+
+def test_transfer_success():
+    transfer_amount = 100
+    result = bill.transfer(from_acc, to_acc, transfer_amount)
+    assert_equals(result, True)
+    assert_equals(checkingAccount.sumTransactions(), 2500.0)
+    assert_equals(savingAccount.sumTransactions(), 1000.0)
+
+@raises(ValueError)
+def test_transfer_insufficient_balance_failure():
+    transfer_amount = 5000
+    result = bill.transfer(from_acc, to_acc, transfer_amount)
+
+@raises(ValueError)
+def test_transfer_insufficient_balance_failure():
+    checkingAccount_oscar = Account(CHECKING)
+    oscar = Customer("Oscar").openAccount(checkingAccount_oscar)
+    bank.addCustomer(oscar)
+    checkingAccount_oscar.deposit(100.0)
+    transfer_amount = 5000
+    from_acc = checkingAccount_oscar
+    to_acc = savingAccount
+    result = bill.transfer(from_acc, to_acc, transfer_amount)
+

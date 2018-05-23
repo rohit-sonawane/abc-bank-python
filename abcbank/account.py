8a7ffacdf3f901e2bb6e61748a5be3e7cdb24fb2
Rohit_Sonawane <rohit2790@gmail.com>
2018-05-22 23:10:22 -0400

    adding functionality for max saving account interest change

--- abcbank/account.py
+++ abcbank/account.py
@@ -1,4 +1,5 @@
-from abcbank.transaction import Transaction
+from transaction import Transaction
+from date_provider import DateProvider
 
 CHECKING = 0
 SAVINGS = 1
@@ -20,24 +21,31 @@ class Account:
         if (amount <= 0):
             raise ValueError("amount must be greater than zero")
         else:
-            self.transactions.append(Transaction(-amount))
+            if self.sumTransactions() >= amount:
+                self.transactions.append(Transaction(-amount))
+            else:
+                raise ValueError("Not sufficient amount in the account to withdraw")
 
     def interestEarned(self):
-        amount = self.sumTransactions()
+        days_in_year = 365
+        actual_amount = self.sumTransactions()
+        amount = actual_amount
         if self.accountType == SAVINGS:
-            if (amount <= 1000):
-                return amount * 0.001
-            else:
-                return 1 + (amount - 1000) * 0.002
+            for day in range((self.transactions[-1].transactionDate - self.transactions[0].transactionDate).days):
+                if amount <= 1000:
+                    amount += amount * 0.001/days_in_year
+                else:
+                    amount += 1 + (amount - 1000) * 0.002/days_in_year
         if self.accountType == MAXI_SAVINGS:
-            if (amount <= 1000):
-                return amount * 0.02
-            elif (amount <= 2000):
-                return 20 + (amount - 1000) * 0.05
-            else:
-                return 70 + (amount - 2000) * 0.1
+            for each_transaction in self.transactions:
+                if (DateProvider.now() - each_transaction.transactionDate).days <= 10:
+                    amount += amount * 0.001/days_in_year
+                else:
+                    amount += amount * 0.05/days_in_year
         else:
-            return amount * 0.001
+            for day in range((self.transactions[-1].transactionDate - self.transactions[0].transactionDate).days):
+                amount += amount * 0.001/days_in_year
+        return amount - actual_amount
 
     def sumTransactions(self, checkAllTransactions=True):
         return sum([t.amount for t in self.transactions])
